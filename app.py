1675dbb3287be2bc9cdaae5951d5d134100473a3
AbA1oNe <louis2001145145@gmail.com>
2023-04-26 00:48:06 +0800

    model and receive data

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify, url_for, redirect
 from flask_login import login_user, logout_user, login_required
+from flask_login import current_user
 import numpy as np
 import pickle
 from initiate import app, db
 from initiate.login_models import User
+import json
 
 model = pickle.load(open('model.pkl','rb'))
 
@@ -21,19 +23,23 @@ def login():
         redirect(url_for('register'))
         return f"no such username {username}"
     if user.check_password(password) and user is not None:
-        login_user(user)
+        login_user(user, remember=True)
         next = request.args.get('next')
         if next == None or not next[0]=='/':
             next = url_for('index')
     else:
         redirect(url_for('login'))
         return "wrong password"
+    
+    print(f"User{current_user.usernmae} has logged in")
     return redirect(next)
 
-@app.route('/logout')
+@app.route('/logout',methods=['POST'])
 @login_required
 def logout():
+    print(f'User {current_user.username} has logged out')
     logout_user()
+    
     return redirect(url_for('index'))
 
 @app.route('/register',methods=['POST'])
@@ -49,6 +55,28 @@ def register():
         redirect(url_for('register'))
         return f"username {username} already exist"
     
+    print(f'User {username} registered')
+    return redirect(url_for('index'))
+
+
+@app.route('/receive', methods=['POST'])
+@login_required
+def receiveData():
+    username = current_user.username
+    if (request.is_json):
+        data = request.get_json()
+        with open('./data.json', 'r+') as file:
+            fileData = json.load(file)
+            if (username in fileData):
+                fileData[username].append(data['value'])
+                file.seek(0)
+                json.dump(fileData, file, indent=2)
+            else:
+                fileData[username] = data['value']
+                file.seek(0)
+                json.dump(fileData, file, indent=2)
+    
+    print(f"User {current_user.username} sends {data['value']}")
     return redirect(url_for('index'))
 
 @app.route('/predict',methods=['POST'])
