44b6a85f23a2a82c3f9b1a0b78ae48ce1ea63afc
AbA1oNe <louis2001145145@gmail.com>
2023-10-17 00:33:04 +0800

    backup

--- train.py
+++ train.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import json
 
-def transform(): #load json into dataframe
-    with open('data.json', 'r') as jsonFile:
+def transform(file): #load json into dataframe
+    with open(file, 'r') as jsonFile:
         data = json.load(jsonFile)
         numSubject = len(data)
         dfList = []
@@ -39,6 +39,45 @@ def transform(): #load json into dataframe
                 
     return dfList
 
+def onlyOneUser(file, username):
+    with open(file, 'r') as jsonFile:
+        data = json.load(jsonFile)
+        df = pd.DataFrame()
+        df1 = pd.DataFrame()
+        
+        for col in data[next(iter(data))][0]: #initialize the columns of the dataframe
+            if (col != "timestamp"):
+                df[col] = ""
+                df1[col] = ""
+
+        print(len(data[username]))
+        for dict in data[username]:
+            for key, value in dict.items():
+                if (key != "timestamp" and key != "label"):
+                    df1[key] = [value]
+                elif key == "label":
+                    df1[key] = 1
+            df = pd.concat([df,df1])
+
+    return df
+    
+def addNewData():
+    userList = ["1092954", "dama0623", "1094908", "4109034029", "611034", "1094841", "D1186959",
+                "411411159", "1094845", "1094842", "110", "pomiii5093", "1092574", "anyu5471", "px",
+                "wardlin", "lenny", "1092960", "1092923", "1092950", "1092942", "1092928", "1092922"]
+    
+    dfList = transform("data.json")
+    with open("temp.json", 'r') as jsonFile:
+        data = json.load(jsonFile)
+        for user in data:
+            index = userList.index(user)
+            df = pd.DataFrame()
+            df = onlyOneUser("temp.json", user).sample(frac=0.5)
+            
+            dfList[index] = pd.concat([dfList[index], df])
+    
+    return dfList
+    
 pd.set_option('display.max_colwidth', None)
 pd.set_option('display.max_rows', None)
 #data = transform()
